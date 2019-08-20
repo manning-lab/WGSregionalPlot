a348d90248ae921c7a390d0c45b32f7957c99d58
tmajaria <tmajaria@broadinstitute.org>
2019-08-20 16:43:17 -0400

    update exon plotting

--- R/make_gene_track.R
+++ R/make_gene_track.R
@@ -10,7 +10,7 @@
 #' @param background_frame whether to add frame
 #' @return \code{gene.track} gviz track
 
-#' @importFrom Gviz AnnotationTrack
+#' @importFrom Gviz AnnotationTrack GeneRegionTrack
 #' @importFrom biomaRt getBM
 
 make_gene_track <- function(chr, start, end, mart,
@@ -19,40 +19,58 @@ make_gene_track <- function(chr, start, end, mart,
                           title = "Genomic Context",
                           background_color = "#31a354",
                           background_frame = T){
-  # query biomart for the gene positions and symbols
-  gene.data <- getBM(attributes=c("ensembl_gene_id", "hgnc_symbol", "chromosome_name", "start_position", "end_position"),filters = c("chromosome_name"), values=list(chr), mart=mart)
 
-  # subset to genes in the region
-  gene.data <- gene.data[(gene.data$end_position >= start) & (gene.data$start_position <= end),]
-  # gene.data[gene.data$hgnc_symbol == "", "hgnc_symbol"] <- gene.data[gene.data$hgnc_symbol == "", "ensembl_gene_id"]
-  gene.data <- gene.data[gene.data$hgnc_symbol != "",]
+  depricated <- T
+  if (!depricated){
+    # query biomart for the gene positions and symbols
+    gene.data <- getBM(attributes=c("ensembl_gene_id", "hgnc_symbol", "chromosome_name", "start_position", "end_position"),filters = c("chromosome_name"), values=list(chr), mart=mart)
 
-  # define a color for non highlighted genes (tan)
-  gene.data$color <- "#ffd470"
+    # subset to genes in the region
+    gene.data <- gene.data[(gene.data$end_position >= start) & (gene.data$start_position <= end),]
+    # gene.data[gene.data$hgnc_symbol == "", "hgnc_symbol"] <- gene.data[gene.data$hgnc_symbol == "", "ensembl_gene_id"]
+    gene.data <- gene.data[gene.data$hgnc_symbol != "",]
 
-  # change the color of highlighted genes
-  if (!is.null(highlight)){
-    highlight <- c(highlight)
-    gene.data[gene.data$hgnc_symbol %in% highlight, "color"] <- "#de2d26"
+    # define a color for non highlighted genes (tan)
+    gene.data$color <- "#ffd470"
+
+    # change the color of highlighted genes
+    if (!is.null(highlight)){
+      highlight <- c(highlight)
+      gene.data[gene.data$hgnc_symbol %in% highlight, "color"] <- "#de2d26"
+    }
+
+    gene.track <- AnnotationTrack(
+      name = title,
+      start = gene.data$start_position,
+      end = gene.data$end_position,
+      id = gene.data$hgnc_symbol,
+      feature = gene.data$color,
+      chromosome = gene.data$chromosome_name[1],
+      genome = genome_build,
+      col="transparent",
+      background.title = background_color,
+      col.frame = background_color,
+      frame = background_frame,
+      groupAnnotation="id",
+      "#ffd470" = "#ffd470",
+      "#0fa3ff" = "#0fa3ff",
+      "#de2d26" = "#de2d26"
+    )
+  } else {
+    gene.track <- BiomartGeneRegionTrack(
+      name = title,
+      start = start,
+      end = end,
+      chromosome = gene.data$chromosome_name[1],
+      genome = genome_build,
+      background.title = background_color,
+      col.frame = background_color,
+      frame = background_frame,
+      collapseTranscripts="meta",
+      transcriptAnnotation="symbol",
+      biomart = mart
+      # shape="arrow",
+    )
   }
-
-  gene.track <- AnnotationTrack(
-    name = title,
-    start = gene.data$start_position,
-    end = gene.data$end_position,
-    id = gene.data$hgnc_symbol,
-    feature = gene.data$color,
-    chromosome = gene.data$chromosome_name[1],
-    genome = genome_build,
-    col="transparent",
-    background.title = background_color,
-    col.frame = background_color,
-    frame = background_frame,
-    groupAnnotation="id",
-    "#ffd470" = "#ffd470",
-    "#0fa3ff" = "#0fa3ff",
-    "#de2d26" = "#de2d26"
-  )
-  # displayPars(gene.track) <- list(fontsize.group = 10)
   return(gene.track)
 }

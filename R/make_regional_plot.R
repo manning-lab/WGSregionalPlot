f6e821296cfe5271d5f349d879d09f6b151009e1
tmajaria <tmajaria@broadinstitute.org>
2019-08-26 11:36:06 -0400

    remove title on axis track

--- R/make_regional_plot.R
+++ R/make_regional_plot.R
@@ -137,7 +137,8 @@ make_regional_plot <- function(chr, start, end, variant_data, variant_chr_column
                                  chromosome = chr,
                                  scale = round((end - start)/10, -2),
                                  labelPos="below",
-                                 cex = 2)
+                                 cex = 2,
+                                 main = " ")
 
   # make idiogram track
   idiogram.track <- IdeogramTrack(genome = genome_build,
